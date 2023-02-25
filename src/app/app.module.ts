78a073140e76819cc04ab0a9e62be09fcc4e62d2
akshay <akshaypaik@gmail.com>
2023-02-25 11:13:48 +0530

    initial commit kickstarting interview smasher

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GlobalHeaderComponent } from './global-header/global-header.component';
+import { GlobalFooterComponent } from './global-footer/global-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GlobalHeaderComponent,
+    GlobalFooterComponent
   ],
   imports: [
     BrowserModule,
